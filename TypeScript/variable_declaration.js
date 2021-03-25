382c081b321e3ff6eff12f0c27a8d5d294a6735d
kajal7002 <kajal7002@gmail.com>
2021-03-25 17:44:00 -0500

    my portfolio

---
+++ TypeScript/variable_declaration.js
@@ -0,0 +1,16 @@
+var a=10;
+var a=20;
+console.log(a);
+console.log(a);
+let b=30;
+//let b=40;
+for(var i=0;i<100;i++){
+
+}
+console.log(i);
+for(let j=0;j<100;j++){
+
+}
+//console.log(j);
+const c=100;
+//c=200;
